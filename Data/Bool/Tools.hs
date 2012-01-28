bb3eefbfbd4300a24ea6e69216cae3a93728ebe0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2012-01-28 16:46:13 +0900

    add whether

--- Data/Bool/Tools.hs
+++ Data/Bool/Tools.hs
@@ -1,6 +1,7 @@
 module Data.Bool.Tools (
   (&&&),
-  (|||)
+  (|||),
+  whether
 ) where
 
 (&&&), (|||) :: Monad m => m Bool -> m Bool -> m Bool
@@ -10,3 +11,7 @@ p1 &&& p2 = do b1 <- p1
 p1 ||| p2 = do b1 <- p1
                b2 <- p2
 	       return $ b1 || b2
+
+whether :: a -> a -> Bool -> a
+whether x _ True = x
+whether _ y False = y
