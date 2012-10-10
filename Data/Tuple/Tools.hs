a26736219c0576f487011b0bfde88b9a039c531e
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-10-10 14:40:42 +0900

    uncurry3 and curry3 should be at Data.Tuple.Tools

--- Data/Tuple/Tools.hs
+++ Data/Tuple/Tools.hs
@@ -1,6 +1,14 @@
 module Data.Tuple.Tools (
+	curry3,
+	uncurry3,
 	rotate
 ) where
+
+curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
+curry3 f x y z = f (x, y, z)
+
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (x, y, z) = f x y z
 
 rotate :: Floating a => a -> (a, a) -> (a, a)
 rotate rad (x, y) = (x * cos rad - y * sin rad, x * sin rad + y * cos rad)
